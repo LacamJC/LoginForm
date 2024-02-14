823d19f8f0d4ac754ecb8227c6592fb8fefe94fd
LacamJC <joaoramajo744@gmail.com>
2024-02-14 20:12:44 -0300

    Add verify password and username

--- views/verficaLogin.php
+++ views/verficaLogin.php
@@ -4,17 +4,11 @@ require_once '../connection.php';
 // Certifique-se de validar e filtrar as entradas do usuário
 $username = $_GET['username'];
 $password = $_GET['password'];
-$sql = 'SELECT * FROM users WHERE user_username = ? LIMIT 1';
+$sql = 'SELECT * FROM users where user_username = ? and user_password = ? LIMIT 1';
 $stmt = $conn->prepare($sql);
 $stmt->execute([$username]);
 $resultados = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($resultados) {
-    // Autenticação bem-sucedida
-    // Armazene informações do usuário em sessão ou cookie
-    // Redirecione para a página de login bem-sucedida
-
-
-
     $_SESSION['username'] = $resultados['user_username'];
     $_SESSION['password'] = $resultados['user_password'];
 
