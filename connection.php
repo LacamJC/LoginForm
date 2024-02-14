208f2e2286f14005125ef4c4555c55cdccb4037c
LacamJC <joaoramajo744@gmail.com>
2024-02-14 19:31:40 -0300

    Add railway

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$username = 'ramajo';
-$password = '123456';
-$dbName = 'LoginForm';
+$username = 'root';
+$password = 'fFDfhfaHedH-hD1eHh5CFHGDH4dfbH5e';
+$dbName = 'railway';
 
 try{
-    $conn = new PDO('mysql:host=127.0.0.1;dbname='.$dbName, $username, $password);
+    $conn = new PDO('mysql:host=roundhouse.proxy.rlwy.net;port=37019;dbname='.$dbName, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("Não foi possível se conectar com o banco de dados.");
