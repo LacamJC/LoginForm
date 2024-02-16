8830b94e87ba3bd4241e902750fe804a59e20b3f
LacamJC <joaoramajo744@gmail.com>
2024-02-16 16:15:38 -0300

    Add scss paste

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <title>Welcome to Login Forms</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="shortcut icon" href="public/img/0-square-fill.svg" type="image/x-icon">
-    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/scss/main.css">
   </head>
   <body>
 
